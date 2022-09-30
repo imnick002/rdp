87666ec1baa63894b63bcb1a90efc48085550288
imnick002 <44601577+imnick002@users.noreply.github.com>
2022-09-30 17:37:31 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -138,8 +138,10 @@ def runNgrok():
     clear_output()
 
     print("ssh", user, end='@')
-    ! curl -s http://localhost:4040/api/tunnels | python3 -c \
-            "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'][6:].replace(':', ' -p '))"
+    
+    #! curl -s http://localhost:4040/api/tunnels | python3 -c \
+    
+           # "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'][6:].replace(':', ' -p '))"
 
 try:
     user = username
